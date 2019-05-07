15d5dea74193c25928e890f5eb383fd1923fa8fc
sandeep <sandeepblh@gmail.com>
2019-05-08 09:54:42 +1000

    initial commit

---
+++ src/main/java/org/gitsandyblr/stocktimeseries/StockTimeSeriesApplication.java
@@ -0,0 +1,13 @@
+package org.gitsandyblr.stocktimeseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTimeSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTimeSeriesApplication.class, args);
+	}
+
+}
